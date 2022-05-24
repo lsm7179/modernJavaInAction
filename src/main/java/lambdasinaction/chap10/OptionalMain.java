0b8622072cd2cee66c37c1a9c2b34a1b3680411d
이승민 <sml7179@naver.com>
2022-05-24 22:38:44 +0900

    10장 optional 정리

--- src/main/java/lambdasinaction/chap10/OptionalMain.java
+++ src/main/java/lambdasinaction/chap10/OptionalMain.java
@@ -21,4 +21,17 @@ public class OptionalMain {
                       .flatMap(Optional::stream)
                       .collect(toSet());
     }
+
+    public String sumString(String a, String b){
+        return a+b;
+    }
+
+    public Optional<String> sumOptString(){
+        Optional<String> nicky = Optional.of("이승민");
+        Optional<String> lucky = Optional.of("운이좋네");
+
+        return nicky.flatMap(n -> lucky.map(l -> sumString(n,l)));
+    }
+
+
 }
